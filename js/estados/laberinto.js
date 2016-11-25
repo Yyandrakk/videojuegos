a2e5a7aa951e257bea7f0e3b61172c32c5431820
jajuro <javier.juarezr90@gmail.com>
2016-11-25 16:12:20 +0100

    Add files via upload

--- js/estados/laberinto.js
+++ js/estados/laberinto.js
@@ -1,4 +1,4 @@
-define(['Phaser','Game','sprites/dog'], function (Phaser,Game,Player) {
+define(['Phaser','Game','sprites/dog'], function (Phaser,Game,Dog) {
 
     function Laberinto() {
         Phaser.State.call(this);
@@ -15,36 +15,38 @@ define(['Phaser','Game','sprites/dog'], function (Phaser,Game,Player) {
         this.dog.anchor.setTo(0.5, 0.5);
 
         this.dog.scale.setTo(0.75,0.75);
+        this.listSprites.push(this.dog);
         Game.physics.arcade.enable(this.dog);
         //Game.camera.follow(this.player);
         this.listSprites.push(this.dog);
     }
     Laberinto.prototype.create = function () {
         Game.world.setBounds(0,0,40*32,40*32);
-
+        Game.camera.follow(this.dog);
 
     }
-    Mundo.prototype.update = function () {
-        Game.physics.arcade.collide(this.dog,this.muro);
-        Game.physics.arcade.collide(this.player,this.decoracion);
+    Laberinto.prototype.update = function () {
+        Game.physics.arcade.collide(this.dog,this.arboles);
+        Game.physics.arcade.collide(this.dog,this.objetivo);
         for(i=0;i<this.listSprites.length;i++){
             this.listSprites[i].update();
         }
 
     }
-    Mundo.prototype.createWorld = function () {
-        this.map=Game.add.tilemap('mapLab');
+    Laberinto.prototype.createWorld = function () {
+        this.map=Game.add.tilemap('mapLab', tileWidth=50, tileHeight=50);
+        
         this.map.addTilesetImage('tora_vx_02');
         this.map.addTilesetImage('mountain_landscape');
 
         this.suelo = this.map.createLayer('Capa suelo');
         this.suelo.resizeWorld();
-        this.paredes = this.map.createLayer('Capa Arboles');
+        this.arboles = this.map.createLayer('Capa Arboles');
         this.objetivo = this.map.createLayer('Capa Objetivo');
-        //this.map.setCollisionBetween(1, 10000, true, this.muro);
-        //this.map.setCollisionBetween(1, 10000, true, this.decoracion);
+        this.map.setCollisionBetween(1, 10000, true, this.arboles);
+        this.map.setCollisionBetween(1, 10000, true, this.objetivo);
 
 
     }
-    return Mundo;
+    return Laberinto;
 });
