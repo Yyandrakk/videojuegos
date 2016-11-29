13dcf2d591404207d3bacafddc717ff7fd37ea59
jajuro <javier.juarezr90@gmail.com>
2016-11-29 10:58:33 +0100

    Add files via upload

--- js/sprites/dog.js
+++ js/sprites/dog.js
@@ -9,27 +9,60 @@ define(['Phaser'], function (Phaser) {
         this.animations.play('down');
         this.anchor.setTo(0.5,0.5);
         game.add.existing(this);
-        game.input.mouse.capture = true;
+        //game.input.mouse.capture = true;
         //this.cursor = game.input.keyboard.createCursorKeys();
-
+        
+        this.cursor= {
+            up: [game.input.keyboard.addKey(Phaser.Keyboard.W), game.input.keyboard.addKey(Phaser.Keyboard.UP)],
+            left: [game.input.keyboard.addKey(Phaser.Keyboard.A), game.input.keyboard.addKey(Phaser.Keyboard.LEFT)],
+            right: [game.input.keyboard.addKey(Phaser.Keyboard.D), game.input.keyboard.addKey(Phaser.Keyboard.RIGHT)],
+            down: [game.input.keyboard.addKey(Phaser.Keyboard.S), game.input.keyboard.addKey(Phaser.Keyboard.DOWN)]
+        }
+         game.add.existing(this);
     }
 
 
     Dog.prototype = Object.create(Phaser.Sprite.prototype);
     Dog.prototype.constructor = Dog;
     Dog.prototype.update = function () {
-        if (game.input.mousePointer.isDown) {
-            game.physics.arcade.moveToPointer(this.body, 400);
-            if (Phaser.Rectangle.contains(this.body, game.input.x, game.input.y))
-                this.body.velocity.setTo(0, 0);
+        if (this.cursor.left.some(bt => bt.isDown==true)) {
+            this.body.velocity.x = -200;
+            this.body.velocity.y = 0;
+            this.animations.play('left');
+        }
+        else if (this.cursor.right.some(bt => bt.isDown==true)) {
+            this.body.velocity.x = 200;
+            this.body.velocity.y = 0;
+            this.animations.play('derecha');
+        }
+        else if(this.cursor.up.some(b => b.isDown==true)){
+            this.body.velocity.y = -200;
+            this.body.velocity.x = 0;
+            this.animations.play('up');
+        }
+        else if(this.cursor.down.some(b=>b.isDown==true)){
+            this.body.velocity.y = +200;
+            this.body.velocity.x = 0;
+            this.animations.play('down');
         }
         else {
             this.body.velocity.x = 0;
             this.body.velocity.y = 0;
             this.animations.stop();
-            //this.player.frame = 1;
         }
     }
+        //if (game.input.mousePointer.isDown) {
+        //    game.physics.arcade.moveToPointer(this.body, 400);
+        //    if (Phaser.Rectangle.contains(this.body, game.input.x, game.input.y))
+        //        this.body.velocity.setTo(0, 0);
+        //}
+        //else {
+        //    this.body.velocity.x = 0;
+        //    this.body.velocity.y = 0;
+        //    this.animations.stop();
+            //this.player.frame = 1;
+        //}
+    //}
 
     return Dog;
 });
