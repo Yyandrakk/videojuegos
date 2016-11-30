6e633968f7fa482f3bec8d13791db806ad2aca1a
jajuro <javier.juarezr90@gmail.com>
2016-11-30 10:52:57 +0100

    Add files via upload

--- js/estados/laberinto.js
+++ js/estados/laberinto.js
@@ -1,4 +1,4 @@
-define(['Phaser','Game','sprites/dog'], function (Phaser,Game,Dog) {
+define(['Phaser','Game','sprites/dog','estados/mundo'], function (Phaser,Game,Dog,Mundo) {
 
     function Laberinto() {
         Phaser.State.call(this);
@@ -17,7 +17,6 @@ define(['Phaser','Game','sprites/dog'], function (Phaser,Game,Dog) {
         this.dog.scale.setTo(0.6,0.6);
         this.listSprites.push(this.dog);
         Game.physics.arcade.enable(this.dog);
-        //Game.camera.follow(this.player);
         this.listSprites.push(this.dog);
     }
     Laberinto.prototype.create = function () {
@@ -25,7 +24,12 @@ define(['Phaser','Game','sprites/dog'], function (Phaser,Game,Dog) {
         this.dog.position.x = 80;
         this.dog.position.y = 810;
         Game.camera.follow(this.dog);
-
+        this.numVidas = this.game.add.text(30, 30, 'Vidas: 3',{ font: '18px Arial', fill: '#ffffff' });
+        this.numVidas.fixedToCamera = true;
+        this.deathSound = this.game.add.audio('dogdeath');
+        this.music = this.game.add.audio('mazemusic');
+        this.music.loop = true;
+        this.music.play();
     }
     Laberinto.prototype.update = function () {
         Game.physics.arcade.collide(this.dog,this.arboles, this.dogDie, null, this);
@@ -54,10 +58,19 @@ define(['Phaser','Game','sprites/dog'], function (Phaser,Game,Dog) {
     Laberinto.prototype.dogDie = function() {
         this.dog.position.x = 80;
         this.dog.position.y = 810;
+        Game.global.control.laberinto.vidas -= 1;
+        this.numVidas.text = 'Vidas: ' + Game.global.control.laberinto.vidas;
+        if (Game.global.control.laberinto.vidas < 0){
+            //Game.state.start('GameOver');
+            Game.state.start('Menu');
+            this.music.stop();
+        }
+        this.deathSound.play();
     }
     
     Laberinto.prototype.levelCompleted = function() {
         Game.global.control.laberinto.haGanado = true;
+        Game.state.add('Mundo', new Mundo());
         Game.state.start('Mundo');
     }
     return Laberinto;
