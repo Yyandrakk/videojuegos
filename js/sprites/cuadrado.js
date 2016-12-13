d8de2932b78ef314e103829d018fde03459a7b2e
jajuro <javier.juarezr90@gmail.com>
2016-12-13 15:59:42 +0100

    Add files via upload

---
+++ js/sprites/cuadrado.js
@@ -0,0 +1,43 @@
+define(['Phaser'], function(Phaser){
+    function Cuadrado(game,x,y,key){
+        Phaser.Sprite.call(this,game,x,y,key);
+        this.anchor.setTo(0.5,0.5);
+        game.add.existing(this);
+        this.cursor = game.input.keyboard.createCursorKeys();
+        game.load.spritesheet('cuadrado', key, 300, 280);
+        
+        this.cursor= {
+            up: [game.input.keyboard.addKey(Phaser.Keyboard.W), game.input.keyboard.addKey(Phaser.Keyboard.UP)],
+            left: [game.input.keyboard.addKey(Phaser.Keyboard.A), game.input.keyboard.addKey(Phaser.Keyboard.LEFT)],
+            right: [game.input.keyboard.addKey(Phaser.Keyboard.D), game.input.keyboard.addKey(Phaser.Keyboard.RIGHT)],
+            down: [game.input.keyboard.addKey(Phaser.Keyboard.S), game.input.keyboard.addKey(Phaser.Keyboard.DOWN)]
+        }
+    }
+    
+    Cuadrado.prototype = Object.create(Phaser.Sprite.prototype);
+    Cuadrado.prototype.constructor = Cuadrado;
+    
+    Cuadrado.prototype.update = function () {
+        if (this.cursor.left.some(bt => bt.isDown==true)) {
+            this.body.velocity.x = -200;
+            this.body.velocity.y = 0;
+        }
+        else if (this.cursor.right.some(bt => bt.isDown==true)) {
+            this.body.velocity.x = 200;
+            this.body.velocity.y = 0;
+        }
+        else if(this.cursor.up.some(b => b.isDown==true)){
+            this.body.velocity.y = -200;
+            this.body.velocity.x = 0;
+        }
+        else if(this.cursor.down.some(b=>b.isDown==true)){
+            this.body.velocity.y = +200;
+            this.body.velocity.x = 0;
+        }
+        else {
+            this.body.velocity.x = 0;
+            this.body.velocity.y = 0;
+            this.animations.stop();
+        }
+    }
+});
