b8fe1e8720d43dc440032218206335daab51805f
jajuro <javier.juarezr90@gmail.com>
2016-12-31 11:46:30 +0100

    Add files via upload

--- js/estados/puzzle.js
+++ js/estados/puzzle.js
@@ -5,62 +5,128 @@ define(['Phaser','Game','sprites/cuadrado','estados/mundo'], function(Phaser,Gam
         this.listSprites = [];
     }
     
+	var ANCHO_PIEZA = 100, ALTO_PIEZA = 93.33333;
+	var totalPiezas, grupoPiezas, aleat = [];
     //Inheritance
     Puzzle.prototype = Object.create(Phaser.State.prototype);
     Puzzle.prototype.constructor = Puzzle;
     
     Puzzle.prototype.preload = function(){
-        this.cuadrado1 = new Cuadrado(Game,0,0,'cuadrado1');
-        this.cuadrado2 = new Cuadrado(Game,30,30,'cuadrado2');
-        this.cuadrado3 = new Cuadrado(Game,0,0,'cuadrado3');
-        this.cuadrado4 = new Cuadrado(Game,0,0,'cuadrado4');
-        //this.cuadrado5 = new Cuadrado(Game,0,0,'cuadrado5');
-        this.cuadrado6 = new Cuadrado(Game,0,0,'cuadrado6');
-        this.cuadrado7 = new Cuadrado(Game,0,0,'cuadrado7');
-        this.cuadrado8 = new Cuadrado(Game,0,0,'cuadrado8');
-        this.cuadrado9 = new Cuadrado(Game,0,0,'cuadrado9');
-        
-        this.listSprites.push(this.cuadrado1);
-        this.listSprites.push(this.cuadrado2);
-        this.listSprites.push(this.cuadrado3);
-        this.listSprites.push(this.cuadrado4);
-        //this.listSprites.push(this.cuadrado5);
-        this.listSprites.push(this.cuadrado6);
-        this.listSprites.push(this.cuadrado7);
-        this.listSprites.push(this.cuadrado8);
-        this.listSprites.push(this.cuadrado9);
-        Game.physics.arcade.enable(this.cuadrado1);
-        Game.physics.arcade.enable(this.cuadrado2);
-        Game.physics.arcade.enable(this.cuadrado3);
-        Game.physics.arcade.enable(this.cuadrado4);
-        //Game.physics.arcade.enable(this.cuadrado5);
-        Game.physics.arcade.enable(this.cuadrado6);
-        Game.physics.arcade.enable(this.cuadrado7);
-        Game.physics.arcade.enable(this.cuadrado8);
-        Game.physics.arcade.enable(this.cuadrado9);
+		Game.load.spritesheet("imagen", "media/sprite/cientificoloco.jpg", ANCHO_PIEZA, ALTO_PIEZA);
     }
     
     Puzzle.prototype.create = function(){
-        this.cuadrado1.position.x = Game.width-Game.width+37.5;
-        this.cuadrado1.position.y = Game.height-Game.height+35;
-        this.cuadrado8.position.x = Game.width-Game.width+112.5;
-        this.cuadrado8.position.y = Game.height-Game.height+35;
-        this.cuadrado7.position.x = Game.width-Game.width+187.5;
-        this.cuadrado7.position.y = Game.height-Game.height+35;
-        this.cuadrado9.position.x = Game.width-Game.width+112.5;
-        this.cuadrado9.position.y = Game.height-Game.height+105;
-        this.cuadrado3.position.x = Game.width-Game.width+187.5;
-        this.cuadrado3.position.y = Game.height-Game.height+105;
-        this.cuadrado4.position.x = Game.width-Game.width+37.5;
-        this.cuadrado4.position.y = Game.height-Game.height+175;
-        this.cuadrado2.position.x = Game.width-Game.width+112.5;
-        this.cuadrado2.position.y = Game.height-Game.height+175;
-        this.cuadrado6.position.x = Game.width-Game.width+187.5;
-        this.cuadrado6.position.y = Game.height-Game.height+175;
-        //this.cuadrado5.position.x = Game.width-Game.width+37.5;
-        //this.cuadrado5.position.y = Game.height-Game.height+105;
+		var ind = 0, pieza, k = 0;
+		
+		totalPiezas = 9;
+		aleat = this.crearArrayAleatorio();
+		
+		grupoPiezas = Game.add.group();
+		for (i = 0; i < 3; i++){
+			for (j = 0; j < 3; j++){
+				if (aleat[ind]){
+					pieza = grupoPiezas.create(j*ANCHO_PIEZA, i*ALTO_PIEZA, "imagen", aleat[ind]);
+				} else {
+					pieza = grupoPiezas.create(j*ANCHO_PIEZA, i*ALTO_PIEZA);
+					pieza.vacia = true;
+				}
+				pieza.nombre = 'Pieza' + i.toString() + 'x' + j.toString();
+				pieza.current = ind;
+				pieza.indDest = aleat[ind];
+				pieza.inputEnabled = true;
+				pieza.events.onInputDown.add(this.seleccionarPieza, this);
+				pieza.posX = j;
+				pieza.posY = i;
+				ind++;
+				k++;
+			}
+		}
     }
+	
+	Puzzle.prototype.seleccionarPieza = function(pieza){
+		var huecoVacio = this.vecinoVacio(pieza);
+		
+		if (huecoVacio){
+			this.mover(pieza, huecoVacio);
+		}
+	}
+	
+	Puzzle.prototype.vecinoVacio = function(pieza){
+		var vacioEncontrado = false;
+		
+		grupoPiezas.children.forEach(function(element){
+			if (element.posX === (pieza.posX - 1) && element.posY === pieza.posY && element.vacia || 
+			element.posX === (pieza.posX + 1) && element.posY === pieza.posY && element.vacia ||
+			element.posY === (pieza.posY - 1) && element.posX === pieza.posX && element.vacia ||
+			element.posY === (pieza.posY + 1) && element.posX === pieza.posX && element.vacia){
+				vacioEncontrado = element;
+				return;
+			}
+		});
+		
+		return vacioEncontrado;
+	}
+	
+	Puzzle.prototype.mover = function(pieza, huecoVacio){
+		var temp = {
+			posX: pieza.posX,
+			posY: pieza.posY,
+			current: pieza.current
+		};
+		
+		Game.add.tween(pieza).to({x: huecoVacio.posX * ANCHO_PIEZA, y: huecoVacio.posY * ALTO_PIEZA}, 300, Phaser.Easing.Linear.None, true);
+		
+		pieza.posX = huecoVacio.posX;
+		pieza.posY = huecoVacio.posY;
+		pieza.current = huecoVacio.current;
+		pieza.nombre = 'Pieza' + huecoVacio.posX.toString() + 'x' + huecoVacio.posY.toString();
+		
+		huecoVacio.posX = temp.posX;
+		huecoVacio.posY = temp.posY;
+		huecoVacio.current = temp.current;
+		huecoVacio.nombre = 'Pieza' + huecoVacio.posX.toString() + 'x' + huecoVacio.posY.toString();
+		
+		this.fin();
+	}
+	
+	Puzzle.prototype.fin = function(){
+		var esFin = true;
+		
+		grupoPiezas.children.forEach(function(element){
+			if (element.current !== element.indDest){
+				esFin = false;
+				return;
+			}
+		});
+		
+		if (esFin){
+			Game.add.text(Game.world.centerX, Game.world.centerY, "Victoria");
+		}
+	}
+	
+	Puzzle.prototype.crearArrayAleatorio = function(){
+		var indArray = [];
+		for (var i = 0; i < totalPiezas; i++){
+			indArray.push(i);
+		}
+		return this.mezclar(indArray);
+	}
     
+	Puzzle.prototype.mezclar = function(array){
+		var cont = array.length, temp, indice;
+		
+		while (cont > 0){
+			indice = Math.floor(Math.random() * cont);
+			
+			cont--;
+			
+			temp = array[cont];
+			array[cont] = array[indice];
+			array[indice] = temp;
+		}
+		
+		return array;
+	}
     
     return Puzzle;
 });
